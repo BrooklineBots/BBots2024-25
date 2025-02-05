89ef316322a8b4660166e1af578a291758c64931
Mallen220 <68817732+Mallen220@users.noreply.github.com>
2025-02-05 10:12:08 -0500

    Update WebcamExample2.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WebcamExample2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WebcamExample2.java
@@ -1,13 +1,10 @@
 package org.firstinspires.ftc.teamcode;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
@@ -16,7 +13,7 @@ import org.openftc.easyopencv.OpenCvPipeline;
 import org.openftc.easyopencv.OpenCvWebcam;
 
 @TeleOp
-public class KalinasWebcam extends LinearOpMode{
+public class WebcamExample2 extends LinearOpMode {
 
     OpenCvWebcam webcam;
     //purple: 150, 110, 160
@@ -24,23 +21,19 @@ public class KalinasWebcam extends LinearOpMode{
     //orange: 30, 100, 140
 
     @Override
-    public void runOpMode()
-    {
+    public void runOpMode() {
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
         webcam.setPipeline(new SamplePipeline());
         webcam.setMillisecondsPermissionTimeout(2500); // Timeout for obtaining permission is configurable. Set before opening.
-        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
-        {
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
-            public void onOpened()
-            {
+            public void onOpened() {
                 webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
             }
 
             @Override
-            public void onError(int errorCode)
-            {
+            public void onError(int errorCode) {
                 /*
                  * This will be called if the camera could not be opened
                  */
@@ -50,8 +43,7 @@ public class KalinasWebcam extends LinearOpMode{
         telemetry.update();
         waitForStart();
 
-        while (opModeIsActive())
-        {
+        while (opModeIsActive()) {
             telemetry.addData("Frame Count", webcam.getFrameCount());
             telemetry.addData("FPS", String.format("%.2f", webcam.getFps()));
             telemetry.addData("Total frame time ms", webcam.getTotalFrameTimeMs());
@@ -74,17 +66,17 @@ public class KalinasWebcam extends LinearOpMode{
             int ssum = 0;
             int vsum = 0;
 
-            for (int i=0; i<mat.rows(); i++){
-                for (int j=0; j<mat.cols(); j++) {
-                    double[] pixel = mat.get(i,j);
-                    hsum+= pixel[0];
-                    ssum+= pixel[1];
-                    vsum+= pixel[2];
+            for (int i = 0; i < mat.rows(); i++) {
+                for (int j = 0; j < mat.cols(); j++) {
+                    double[] pixel = mat.get(i, j);
+                    hsum += pixel[0];
+                    ssum += pixel[1];
+                    vsum += pixel[2];
                 }
             }
-            int hval = hsum/(mat.rows()*mat.cols());
-            int sval = ssum/(mat.rows()*mat.cols());
-            int vval = vsum/(mat.rows()*mat.cols());
+            int hval = hsum / (mat.rows() * mat.cols());
+            int sval = ssum / (mat.rows() * mat.cols());
+            int vval = vsum / (mat.rows() * mat.cols());
 
             telemetry.addData("H:", hval);
             telemetry.addData("S:", sval);
@@ -95,16 +87,12 @@ public class KalinasWebcam extends LinearOpMode{
         }
 
         @Override
-        public void onViewportTapped()
-        {
+        public void onViewportTapped() {
             viewportPaused = !viewportPaused;
 
-            if(viewportPaused)
-            {
+            if (viewportPaused) {
                 webcam.pauseViewport();
-            }
-            else
-            {
+            } else {
                 webcam.resumeViewport();
             }
         }
