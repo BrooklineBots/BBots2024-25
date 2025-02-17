9e3a00f764e8244cc43e2733ee0fbb1f91a927e2
Mallen220 <allenmc220@gmail.com>
2025-02-17 16:15:09 -0500

    Init

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotContainer.java
@@ -0,0 +1,87 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Subsystems.Arm;
+import org.firstinspires.ftc.teamcode.Subsystems.Claw;
+import org.firstinspires.ftc.teamcode.Subsystems.MecanumDrive;
+import org.firstinspires.ftc.teamcode.autonomous.AutonomousRecorder;
+
+@TeleOp(name = "mainDrive")
+public class RobotContainer extends OpMode {
+
+    private Arm arm;
+    private Claw claw;
+    private MecanumDrive drive;
+
+    private AutonomousRecorder recorder;
+
+    private ElapsedTime recordingTimer;
+    private boolean isRecording = false;
+
+    @Override
+    public void init() {
+        recorder = new AutonomousRecorder(hardwareMap.appContext);
+
+       arm = new Arm(hardwareMap, telemetry);
+       claw = new Claw(hardwareMap, telemetry);
+       drive = new MecanumDrive(hardwareMap, telemetry);
+
+       recordingTimer = new ElapsedTime();
+
+
+    }
+
+    public boolean isWithinTolerance(double targetValue, double currentValue, double tolerance ){
+        return Math.abs(targetValue-currentValue) <= tolerance;
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("Is Recording:", isRecording);
+        this.telemetry.update();
+
+        if (isRecording && recordingTimer.seconds() >= 15.0) {
+            recorder.stopRecording();
+            isRecording = false;
+            gamepad1.rumble(250);
+        }
+
+        if(!isWithinTolerance(0, gamepad1.left_stick_y, 0.1) || !isWithinTolerance(0, gamepad1.left_stick_x, 0.1) || !isWithinTolerance(0, gamepad1.right_stick_x, 0.1)){
+            drive.driveFieldRelative(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
+            recorder.giveCommand("drive.driveFieldRelative(" + gamepad1.left_stick_x + ", " + gamepad1.left_stick_y + ", " + gamepad1.right_stick_x + ");");
+        } else{
+            drive.stop();
+            recorder.giveCommand("drive.stop();");
+        }
+
+        if (!isWithinTolerance(0, gamepad2.left_stick_y, 0.1)) {
+            arm.moveUp(gamepad2.left_stick_y);
+            recorder.giveCommand("arm.moveUp(" + gamepad2.left_stick_y + ");");
+        } else {
+            arm.stop();
+            recorder.giveCommand("arm.stop();");
+        }
+
+        if (gamepad1.a) {
+            claw.closeClaw();
+            recorder.giveCommand("claw.closeClaw();");
+        } else if (gamepad1.b) {
+            claw.openClaw();
+            recorder.giveCommand("claw.openClaw();");
+        }
+
+        if (gamepad1.x) {
+            if (recorder.startRecording()) {
+                isRecording = true;
+                recordingTimer.reset();
+                gamepad1.rumble(250);
+            }
+        }
+
+    }
+
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Claw.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.Constants;
+
+public class Claw {
+
+    private final double MAX_POSITION = 1;
+    private final double MIN_POSITION = 0;
+    private Servo claw;
+
+    private Telemetry telemetry;
+
+    public Claw(HardwareMap hwMap, Telemetry telemetry){
+        this.telemetry = telemetry;
+        claw = hwMap.get(Servo.class, Constants.ClawConstants.CLAW_SERVO_ID);
+        claw.setDirection(Servo.Direction.REVERSE);
+        telemetry.addData("Position:", claw.getPosition());
+    }
+
+    public void openClaw(){
+        claw.setPosition(Constants.ClawConstants.OPEN_POSITION);
+    }
+
+    public void closeClaw(){
+        claw.setPosition(Constants.ClawConstants.CLOSE_POSITION);
+    }
+
+    public void setPosition(double position){
+        if(position >= MIN_POSITION && position <= MAX_POSITION){
+            claw.setPosition(position);
+        }
+    }
+
+    public double getPosition(){
+        return claw.getPosition();
+    }
+
+
+}
