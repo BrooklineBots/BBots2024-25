deb0f60eb4729cfc0dbe3447df065e7d45d42dbf
Mallen220 <allenmc220@gmail.com>
2025-02-17 16:28:03 -0500

    Updated arm code (positions and 2 motors)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -9,7 +9,25 @@ public class Constants {
     }
 
     public static class ArmConstants {
-        public static final String ARM_MOTOR_ID = "tall_linear_actuator";
+        public static final String LEFT_ARM_MOTOR_ID = "left_arm_motor";
+        public static final String RIGHT_ARM_MOTOR_ID = "right_arm_motor";
+        public static final int MIN_POSITION = 0;
+        public static final int MAX_POSITION = 1500;
+        public static final double MOVE_POWER = 0.25;
+        public static final int MAX_ALLOWED_DIFFERENCE = 50;
+    }
+
+    public enum ArmPosition {
+        INTAKE(0),
+        SCORE_LOW(500),
+        SCORE_MID(1000),
+        SCORE_HIGH(1500);
+
+        public final int encoderTicks;
+
+        ArmPosition(int encoderTicks) {
+            this.encoderTicks = encoderTicks;
+        }
     }
 
     public static class ClawConstants {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Arm.java
@@ -5,26 +5,94 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.Constants;
+import org.firstinspires.ftc.teamcode.Constants.ArmPosition;
+import org.firstinspires.ftc.teamcode.Constants.ArmConstants;
 
 public class Arm {
 
-    private DcMotor arm;
-
+    private DcMotor leftArmMotor;
+    private DcMotor rightArmMotor;
     private Telemetry telemetry;
 
-    public Arm(HardwareMap hwMap, Telemetry telemetry){
+    public Arm(HardwareMap hwMap, Telemetry telemetry) {
         this.telemetry = telemetry;
-        arm = hwMap.dcMotor.get(Constants.ArmConstants.ARM_MOTOR_ID);
-        arm.setDirection(DcMotor.Direction.REVERSE);
+
+        leftArmMotor = hwMap.dcMotor.get(ArmConstants.LEFT_ARM_MOTOR_ID);
+        rightArmMotor = hwMap.dcMotor.get(ArmConstants.RIGHT_ARM_MOTOR_ID);
+
+        leftArmMotor.setDirection(DcMotor.Direction.REVERSE);
+        rightArmMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        leftArmMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightArmMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        resetEncoders();
+        setRunMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
-    public void moveUp(double power){
-        arm.setPower(-power);
+    private void setRunMode(DcMotor.RunMode mode) {
+        leftArmMotor.setMode(mode);
+        rightArmMotor.setMode(mode);
     }
 
-    public void stop(){
-        arm.setPower(0);
+    private void resetEncoders() {
+        setRunMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 
+    public void goToPosition(ArmPosition position) {
+        int target = position.encoderTicks;
 
+        if (target < ArmConstants.MIN_POSITION || target > ArmConstants.MAX_POSITION) {
+            telemetry.addData("Arm Error", "Target position %s out of bounds", position.name());
+            telemetry.update();
+            return;
+        }
+
+        leftArmMotor.setTargetPosition(target);
+        rightArmMotor.setTargetPosition(target);
+
+        setRunMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        leftArmMotor.setPower(ArmConstants.MOVE_POWER);
+        rightArmMotor.setPower(ArmConstants.MOVE_POWER);
+
+        telemetry.addData("Arm", "Moving to %s at %d ticks", position.name(), target);
+        telemetry.update();
+    }
+
+    public void moveUp(double power) {
+        setRunMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftArmMotor.setPower(-power);
+        rightArmMotor.setPower(-power);
+        telemetry.addData("Left Arm tick:", leftArmMotor.getCurrentPosition());
+        telemetry.addData("Right Arm tick:", rightArmMotor.getCurrentPosition());
+        telemetry.update();
+    }
+
+    public void stop() {
+        leftArmMotor.setPower(0);
+        rightArmMotor.setPower(0);
+    }
+
+    public boolean isBusy() {
+        return leftArmMotor.isBusy() || rightArmMotor.isBusy();
+    }
+
+    public void update() {
+        int leftPos = leftArmMotor.getCurrentPosition();
+        int rightPos = rightArmMotor.getCurrentPosition();
+
+        if (Math.abs(leftPos - rightPos) > ArmConstants.MAX_ALLOWED_DIFFERENCE) {
+            stop();
+            telemetry.addData("Arm Error", "Motors out of sync by %d ticks", Math.abs(leftPos - rightPos));
+            telemetry.update();
+        }
+
+        if (leftPos < ArmConstants.MIN_POSITION || leftPos > ArmConstants.MAX_POSITION ||
+                rightPos < ArmConstants.MIN_POSITION || rightPos > ArmConstants.MAX_POSITION) {
+            stop();
+            telemetry.addData("Arm Error", "Position out of bounds: Left %d, Right %d", leftPos, rightPos);
+            telemetry.update();
+        }
+    }
 }
