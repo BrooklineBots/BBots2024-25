c994153bef1328db7f1933d1cea84d80fede9508
briellegithub <briellehwang@gmail.com>
2025-06-09 15:42:19 -0400

    Updated horiz. extension to be CRServos (WIP)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/HorizontalExtension.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/HorizontalExtension.java
@@ -1,12 +1,12 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.Constants;
 
 public class HorizontalExtension {
-  private final Servo rightExtensionServo;
+  private final CRServo rightExtensionServo;
   // private final Servo leftExtensionServo;
 
   private final double MAX_POSITION = 1; // TODO: test + change
@@ -19,39 +19,19 @@ public class HorizontalExtension {
     this.hwMap = hwMap;
     this.telemetry = telemetry;
 
-    rightExtensionServo = hwMap.servo.get(Constants.HorizontalConstants.RIGHT_EXTENSION_ID);
+    rightExtensionServo = hwMap.crservo.get(Constants.HorizontalConstants.RIGHT_EXTENSION_ID);
     // leftExtensionServo = hwMap.servo.get(Constants.HorizontalConstants.LEFT_EXTENSION_ID);
-    rightExtensionServo.setDirection(Servo.Direction.REVERSE); // TODO: choose which servo
+    rightExtensionServo.setDirection(CRServo.Direction.REVERSE); // TODO: choose which servo
 
-    telemetry.addData("Right Extension Position: ", rightExtensionServo.getPosition());
     // telemetry.addData("Left Extension Position: ", leftExtensionServo.getPosition());
   }
 
-  public void setRightPosition(double position) {
-    if (position >= MIN_POSITION && position <= MAX_POSITION) {
-      rightExtensionServo.setPosition(position);
-    }
-  }
-
-  // public void getLeftPosition(){leftExtensionServo.getPosition());}
-
-  public double getRightPosition() {
-    return rightExtensionServo.getPosition();
-  }
-
-  //  public void setLeftPosition(double position){
-  //    if (position >= MIN_POSITION && position <= MAX_POSITION) {
-  //      leftExtensionServo.setPosition(position);
-  //    }
-  //  }
 
   public void extendOut() {
-    setRightPosition(Constants.HorizontalPosition.RIGHT_OUT_POSITION.position);
-    // setLeftPosition(Constants.HorizontalPosition.LEFT_OUT_POSITION.position);
+    rightExtensionServo.setPower(1);
   }
 
   public void shrinkBack() {
-    setRightPosition(Constants.HorizontalPosition.RIGHT_IN_POSITION.position);
-    // setLeftPosition(Constants.HorizontalPosition.LEFT_IN_POSITION.position);
+    rightExtensionServo.setPower(-1);
   }
 }
