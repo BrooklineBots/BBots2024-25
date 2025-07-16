1c5d3afb710d3395b432bd1a43f6a9d7fcd20d10
briellegithub <briellehwang@gmail.com>
2025-07-16 19:54:49 -0400

    Cleaned up horizontal extension (DONE)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/HorizontalExtension.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/HorizontalExtension.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode.Subsystems;
 
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Const;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.Constants;
 
@@ -9,13 +11,11 @@ public class HorizontalExtension {
   private final CRServo rightExtensionServo;
   private final CRServo leftExtensionServo;
 
-  private final double MAX_POWER = 1; // TODO: test + change
+  private final double MAX_POWER = 1;
   private final double MIN_POWER = -1;
 
   private final Telemetry telemetry;
   private final HardwareMap hwMap;
-
-  private long startTimeNanoSeconds = -1; // off
 
   public HorizontalExtension(HardwareMap hwMap, Telemetry telemetry) {
     this.hwMap = hwMap;
@@ -23,50 +23,26 @@ public class HorizontalExtension {
 
     rightExtensionServo = hwMap.crservo.get(Constants.HorizontalConstants.RIGHT_EXTENSION_ID);
     leftExtensionServo = hwMap.crservo.get(Constants.HorizontalConstants.LEFT_EXTENSION_ID);
-    rightExtensionServo.setDirection(CRServo.Direction.REVERSE); // TODO: choose which servo
+    rightExtensionServo.setDirection(CRServo.Direction.REVERSE);
   }
 
-  public void setPowerWithDelay(double rightPower, double leftPower) {
-    if (rightPower < MAX_POWER
-        && rightPower > MIN_POWER
-        && leftPower < MAX_POWER
-        && leftPower > MIN_POWER) {
-      if (startTimeNanoSeconds == -1) {
-        leftExtensionServo.setPower(leftPower);
-        startTimeNanoSeconds = System.nanoTime();
-      }
-
-      double elapsedTimeSec = (System.nanoTime() - startTimeNanoSeconds) / 1e9;
-      if (elapsedTimeSec >= Constants.HorizontalConstants.EXTEND_DELAY_SECONDS) {
-        rightExtensionServo.setPower(rightPower);
-      }
-    }
-  }
-
-  public void setPower(double rightPower, double leftPower) {
-    if (rightPower < MAX_POWER
-        && rightPower > MIN_POWER
-        && leftPower < MAX_POWER
-        && leftPower > MIN_POWER) {
-      leftExtensionServo.setPower(leftPower);
-      rightExtensionServo.setPower(rightPower);
+  public void setPower(double power) {
+    if (power < MAX_POWER
+        && power > MIN_POWER) {
+      leftExtensionServo.setPower(power);
+      rightExtensionServo.setPower(power);
     }
   }
 
   public void extend() {
-    setPower(
-        Constants.HorizontalConstants.RIGHT_EXTEND_POWER,
-        Constants.HorizontalConstants.LEFT_EXTEND_POWER);
+    setPower(Constants.HorizontalConstants.EXTENSION_POWER);
   }
 
   public void retract() {
-    setPower(
-        -Constants.HorizontalConstants.RIGHT_RETRACT_POWER,
-        -Constants.HorizontalConstants.LEFT_RETRACT_POWER);
+    setPower(-Constants.HorizontalConstants.EXTENSION_POWER);
   }
 
   public void stopServos() {
-    setPower(0, 0);
-    // startTimeNanoSeconds = -1;
+    setPower(0);
   }
 }
