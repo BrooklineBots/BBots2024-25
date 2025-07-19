24bd96e2467547301cc3bca52b6fe53e6ec09a1c
briellegithub <briellehwang@gmail.com>
2025-07-19 10:51:30 -0400

    Changed intake pickup position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -77,7 +77,7 @@ public class Constants {
 
   public enum IntakePosition {
     FLIP_TRANSFER_POSITION(0.4),
-    FLIP_PICKUP_POSITION(0.93),
+    FLIP_PICKUP_POSITION(0.95),
     CLAW_OPEN_POSITION(0.49),
     CLAW_CLOSE_POSITION(0.52);
 
